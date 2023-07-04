e301c38a0422c0d89e8f622e0450dd41dc98b49a
skopelosbill <119852615+skopelosbill@users.noreply.github.com>
2023-07-04 05:31:21 +0300

    changes 4 July [1]

---
+++ script.js
@@ -0,0 +1,15 @@
+function openNav() {
+    document.getElementById("nav").style.width = "250px";
+   
+    //document.getElementById("display").style.backgroundColor ="rgba(153,74,0,0.9)";
+    //document.getElementById("headtext").style.color ="rgba(153,74,0,0.9)";
+    //document.body.style.backgroundColor = "rgba(153,74,0,0.9)";
+    
+}
+
+function closeNav() {
+    document.getElementById("nav").style.width = "0";
+   //document.getElementById("display").style.backgroundColor ="rgba(255,202,153,1)";
+  //document.getElementById("headtext").style.color ="rgba(255,202,153,1";
+   // document.body.style.backgroundColor = "rgba(255,242,229,1)"; 
+}
