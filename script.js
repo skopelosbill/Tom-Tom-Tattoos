05ebddaaeebfa9551e8f2304b25968405751a964
skopelosbill <119852615+skopelosbill@users.noreply.github.com>
2023-07-19 13:33:33 +0300

    19 July [5]

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 function openNav() {
-    document.getElementById("nav").style.width = "250px";
-   
-    //document.getElementById("display").style.backgroundColor ="rgba(153,74,0,0.9)";
-    //document.getElementById("headtext").style.color ="rgba(153,74,0,0.9)";
-    //document.body.style.backgroundColor = "rgba(153,74,0,0.9)";
-    
+    document.getElementById("nav").style.width = "250px";   
 }
 
 function closeNav() {
     document.getElementById("nav").style.width = "0";
-   //document.getElementById("display").style.backgroundColor ="rgba(255,202,153,1)";
-  //document.getElementById("headtext").style.color ="rgba(255,202,153,1";
-   // document.body.style.backgroundColor = "rgba(255,242,229,1)"; 
 }
