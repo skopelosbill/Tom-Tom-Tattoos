c7089b24679b65ad474d04f284312b3f379857e7
skopelosbill <119852615+skopelosbill@users.noreply.github.com>
2023-09-18 13:22:55 +0300

    18 Sep [4]

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+function myFunction() {
+    var x = document.getElementById("navbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar"
+    }
+}
--- script.js
+++
@@ -1,7 +0,0 @@
-function openNav() {
-    document.getElementById("nav").style.width = "250px";   
-}
-
-function closeNav() {
-    document.getElementById("nav").style.width = "0";
-}
